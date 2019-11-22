4769b2dca5ad99c13ecbc499b0431e4175cc6e5a
Denis Los <los.da@phystech.edu>
2019-11-22 11:50:55 +0300

    Update repository

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RISC-V-High-Performance-Simulator
 RISC-V High Performance Simulator
+
+
+# Cloning
+git clone --recursive https://github.com/denislos/RISC-V-High-Performance-Simulator.git
---
+++ simulator/infra/decoder/decode_tree.h
@@ -0,0 +1,14 @@
+/**
+ * decode_tree.h - Implementation of DecodeTree class
+ */
+
+#ifndef INFRA_DECODE_TREE
+#define INFRA_DECODE_TREE
+
+class DecodeTree
+{
+
+};
+
+
+#endif // INFRA_DECODE_TREE
