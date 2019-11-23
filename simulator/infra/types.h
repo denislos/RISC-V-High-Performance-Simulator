c3223ea4bbf2823fb12f5f4b850f6266faf25eb2
Denis Los <los.da@phystech.edu>
2019-11-23 11:17:44 +0300

    Update types.h

--- simulator/infra/types.h
+++ simulator/infra/types.h
@@ -23,4 +23,16 @@ using uint32 = std::uint32_t;
 using uint64 = std::uint64_t;
 
 
+static const uint8  MAX_VAL8  = UINT8_MAX;
+static const uint16 MAX_VAL16 = UINT16_MAX;
+static const uint32 MAX_VAL32 = UINT32_MAX;
+static const uint64 MAX_VAL64 = UINT64_MAX;
+
+
+static const uint8  NO_VAL8  = 0xBA;
+static const uint16 NO_VAL16 = 0xF00D;
+static const uint32 NO_VAL32 = 0xDEADBEEFul;
+static const uint64 NO_VAL64 = 0xDEADBEEFF00DCAFEull;
+
+
 #endif // INFRA_TYPES_H
